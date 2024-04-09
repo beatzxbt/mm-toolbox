6aceb56cf135aa092eb9b97048dec95e978f933f
Kirua <gregorferrer@gmail.com>
2024-04-09 18:38:23 +0200

    sum moar

--- mm_toolbox/__init__.py
+++ mm_toolbox/__init__.py
@@ -1,1 +1,28 @@
 __VERSION__ = "0.0.2"
+
+
+def clean_repo(repo_name: str) -> None:
+    '''
+    Clean the repository
+    '''
+    import os
+
+    if os.name == "nt":
+        command = f"rmdir build /s /q & rmdir /s /q {repo_name}.egg-info & rmdir /s /q dist"
+    elif os.name == "posix":
+        command = f"rm -r build & rm -r  {repo_name}.egg-info & rm -r dist"
+    else:
+        command = ""
+
+    os.system(command)
+
+
+def install_package(package: str):
+    '''
+    Install desired package
+    '''
+    import pip
+    if hasattr(pip, 'main'):
+        pip.main(['install', package])
+    else:
+        pip._internal.main(['install', package])
