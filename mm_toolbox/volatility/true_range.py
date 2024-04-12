8ccf705b0926a47c6536bbe778f5d1cc3092a3b8
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-04-13 00:56:28 +0100

    add: true range

--- mm_toolbox/volatility/true_range.py
+++ mm_toolbox/volatility/true_range.py
@@ -1,17 +1,36 @@
-import numpy as np
-from numba import njit, int64, float64
+from numba import njit, float64
 
+@njit(["float64(float64, float64, float64)"], cache=True)
+def true_range(open: float, high: float, low: float) -> float:
+    """
+    Calculate the true range of a trading price bar.
 
-@njit(float64(float64, float64, float64), nogil=True)
-def calculate_true_range_f64(open_value: float, high_value: float, low_value: float) -> float:
-    return np.maximum(
-        np.maximum(open_value - low_value, np.abs(high_value - open_value)), np.abs(low_value - open_value)
-    )
+    The true range is the greatest of the following:
+    - The difference between the current high and the current low,
+    - The absolute difference between the current high and the previous close,
+    - The absolute difference between the current low and the previous close.
 
+    Parameters
+    ----------
+    open : float
+        The opening price of the current period.
+        
+    high : float
+        The highest price of the current period.
+        
+    low : float
+        The lowest price of the current period.
 
-@njit(int64(int64, int64, int64), nogil=True)
-def calculate_true_range_i64(open_value: int, high_value: int, low_value: int) -> int:
-    return np.maximum(
-        np.maximum(open_value - low_value, np.abs(high_value - open_value)), np.abs(low_value - open_value)
-    )
+    Returns
+    -------
+    float
+        The true range of the price for the period.
 
+    Examples
+    --------
+    >>> true_range(100.0, 105.0, 95.0)
+    10.0
+
+    Note that the function assumes 'open' as the previous close for calculation purposes.
+    """
+    return max(max(open - low, abs(high - open)), abs(low - open))
