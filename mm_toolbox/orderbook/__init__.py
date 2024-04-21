f1bf1f61a6921007dbb97b1ec995df5efd6a4e6f
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-04-21 20:52:21 +0100

    orderbook resources

---
+++ mm_toolbox/orderbook/__init__.py
@@ -0,0 +1,6 @@
+#    Research Resources
+#    ------------------
+# -> uint vs int for indexing speeds: https://numba.discourse.group/t/uint64-vs-int64-indexing-performance-difference/1500
+# -> binary vs linear search: https://dirtyhandscoding.wordpress.com/2017/08/25/performance-comparison-linear-search-vs-binary-search/
+# -> fastmath for execution order insensitivity: https://numba.readthedocs.io/en/stable/user/performance-tips.html#fastmath
+# -> other stuff already described in the mm_toolbox README :)
