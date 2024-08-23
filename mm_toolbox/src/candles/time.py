077f276fcda4cec652591d8def0356901ccb0d8f
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-08-23 12:56:35 +0300

    fix: incorrect close ts/price behaviour + tests + rename

--- mm_toolbox/src/candles/time.py
+++ mm_toolbox/src/candles/time.py
@@ -3,32 +3,15 @@ from mm_toolbox.src.candles.base import BaseCandles
 
 class TimeCandles(BaseCandles):
     def __init__(self, secs_per_bucket: float, num_candles: int) -> None:
-        self.secs_per_bucket = secs_per_bucket
+        self.millis_per_bucket = secs_per_bucket * 1000.0
         super().__init__(num_candles)
         
     def process_trade(self, timestamp: float, side: bool, price: float, size: float) -> None:
         if self.total_trades == 0.0:
             self.open_timestamp = timestamp
             self.open_price = price
-            self.high_price = price
-            self.low_price = price
-            self.close_price = price
-
-            match side:
-                case 0.0:
-                    self.buy_volume += size
-
-                case 1.0:
-                    self.sell_volume += size
-
-            self.vwap_price = self.calculate_vwap(price, size) 
-            self.total_trades += 1.0
-
-            return None
         
-        if (self.candle_start_time / 1000.0) + self.secs_per_bucket <= timestamp:
-            self.close_timestamp = timestamp
-
+        if self.open_timestamp + self.millis_per_bucket <= timestamp:
             self.insert_candle(
                 open_price=self.open_price,
                 high_price=self.high_price,
@@ -57,3 +40,4 @@ class TimeCandles(BaseCandles):
 
         self.vwap_price = self.calculate_vwap(price, size) 
         self.total_trades += 1.0
+        self.close_timestamp = timestamp
