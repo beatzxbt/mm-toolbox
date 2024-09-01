08f0dc75f65811a0804e565b81e1460bfd251b39
beatzxbt <121855680+beatzxbt@users.noreply.github.com>
2024-09-01 20:55:50 +0300

    add: [complete] high speed ws pool

--- src/mm_toolbox/websocket/client/__init__.py
+++ src/mm_toolbox/websocket/client/__init__.py
@@ -1,17 +1,10 @@
 from .conn import (
-    RawWsPayload as RawWsPayload, 
-    QueuePayload as QueuePayload, 
-    PayloadData as PayloadData, 
-    SingleWsConnection 
+    RawWsPayload as RawWsPayload,
+    QueuePayload as QueuePayload,
+    PayloadData as PayloadData,
+    SingleWsConnection,
 )
 
-from .pool import (
-    WsPoolEvictionPolicy as WsPoolEvictionPolicy,
-    WsPool as WsPool
-)
+from .pool import WsPoolEvictionPolicy as WsPoolEvictionPolicy, WsPool as WsPool
 
-__all__ = [
-    "SingleWsConnection",
-    "WsPoolEvictionPolicy",
-    "WsPool"
-]
+__all__ = ["SingleWsConnection", "WsPoolEvictionPolicy", "WsPool"]
--- src/mm_toolbox/websocket/client/conn.py
+++ src/mm_toolbox/websocket/client/conn.py
@@ -11,6 +11,7 @@ RawWsPayload = bytearray
 PayloadData = Dict[str, Union[int, float, str, Dict, List]]
 QueuePayload = Tuple[int, int, PayloadData]
 
+
 class RawWsConnection(WSListener):
     """
     Wrapper of WSListener class for PicoWs use.
@@ -22,23 +23,24 @@ class RawWsConnection(WSListener):
     transport : WSTransport
         Link to underlying API for sending/recieving messages from buffer.
 
-    process_frame: Callable
+    process_frame : Callable
         Ingesting final bytearray payload. Must take 3 args, process_frame(seq_id, time, payload).
         Payload's memory is shared with the underlying buffer and must be copied in the process.
 
-    final_frame: RawWsPayload
-        A bytearray holding the final websocket frame. May hold partial frames if underlying 
+    final_frame : RawWsPayload
+        A bytearray holding the final websocket frame. May hold partial frames if underlying
         buffer yields one.
     """
+
     def __init__(self, process_frame):
         super().__init__()
-        self.transport: WSTransport = None # For external access
+        self.transport: WSTransport = None  # For external access
         self.process_frame: Callable = process_frame
-        
+
         self.final_frame: RawWsPayload = bytearray()
 
     def on_ws_connected(self, transport: WSTransport):
-        self.transport = transport 
+        self.transport = transport
 
     def on_ws_frame(self, transport: WSTransport, frame: WSFrame):
         self.final_frame += frame.get_payload_as_memoryview()
@@ -48,62 +50,108 @@ class RawWsConnection(WSListener):
             self.final_frame.clear()
 
     # Not used, but available for future impl
-    def on_ws_disconnected(self, transport: WSTransport): pass 
-    def pause_writing(self): pass
-    def resume_writing(self): pass
+    def on_ws_disconnected(self, transport: WSTransport):
+        pass
+
+    def pause_writing(self):
+        pass
+
+    def resume_writing(self):
+        pass
 
 
 class SingleWsConnection:
-    """
-    Manages a single WebSocket connection.
-    """
-
     def __init__(self, logger: Logger) -> None:
+        """
+        Initializes a single WebSocket connection.
+
+        Parameters
+        ----------
+        logger : Logger
+            Logger instance for logging activities.
+        """
         self.logger = logger
         self.running: bool = False
 
         self._ws_client: WSListener = None
         self._seq_id: int = 0
-        self._conn_id = np.random.randint(low=np.iinfo(np.uint16).min, high=np.iinfo(np.uint16).max)
-        self._queue = asyncio.Queue(maxsize=1_000_000)
+        self._conn_id = np.random.randint(
+            low=np.iinfo(np.uint16).min, high=np.iinfo(np.uint16).max
+        )
+        self._queue = asyncio.Queue(maxsize=10_000)
 
     @property
     def seq_id(self) -> int:
+        """
+        Returns the current sequence ID.
+
+        Returns
+        -------
+        int
+            The current sequence ID.
+        """
         return self._seq_id
-    
+
     @property
     def conn_id(self) -> int:
+        """
+        Returns the connection ID.
+
+        Returns
+        -------
+        int
+            The connection ID.
+        """
         return self._conn_id
-    
+
     @property
     def queue(self) -> asyncio.Queue:
+        """
+        Returns the queue associated with this connection.
+
+        Returns
+        -------
+        asyncio.Queue
+            The queue used for incoming messages.
+        """
         return self._queue
 
     def _process_frame(self, time: int, final_frame: RawWsPayload) -> None:
         """
-        Take a frame object, deserialize it into a dict object.
-        Then package it into a known format and insert into queue.
+        Processes an incoming WebSocket frame.
+
+        Parameters
+        ----------
+        time : int
+            The timestamp of the frame.
+
+        final_frame : RawWsPayload
+            The raw WebSocket frame payload.
         """
         try:
             self._seq_id += 1
             q_payload: QueuePayload = (self._seq_id, time, orjson.loads(final_frame))
             self._queue.put_nowait(q_payload)
-        
+
         except orjson.JSONDecodeError:
-            self.logger.warning(f"Unable to decode msg {self._seq_id}: {final_frame.decode()}") 
-            pass
+            self.logger.warning(
+                f"Unable to decode msg {self._seq_id}: {final_frame.decode()}"
+            )
 
         except asyncio.QueueFull:
-            self.logger.warning(f"Conn '{self._conn_id}' queue full, skipping msg {self._seq_id}.")
-            pass
-        
+            self.logger.warning(
+                f"Conn '{self._conn_id}' queue full, skipping msg {self._seq_id}."
+            )
+
     def reset_seq_id(self) -> None:
-        """Set sequence id back to 0"""
+        """
+        Resets the sequence ID to 0.
+        """
         self._seq_id = 0
-        
+
     def send_data(self, payload: Dict) -> None:
         """
-        Send a payload through the WebSocket connection.
+        Sends a payload through the WebSocket connection.
 
         Parameters
         ----------
@@ -115,7 +163,7 @@ class SingleWsConnection:
                 self._ws_client.transport.send(WSMsgType.TEXT, orjson.dumps(payload))
             else:
                 raise ConnectionError(f"Conn '{self._conn_id}' not started yet.")
-            
+
         except orjson.JSONEncodeError as e:
             self.logger.warning(f"Conn '{self._conn_id}' failed to encode: {e}")
 
@@ -128,7 +176,7 @@ class SingleWsConnection:
         on_connect: Optional[List[Dict]] = None,
     ) -> None:
         """
-        Start the WebSocket connection and handle incoming messages.
+        Starts the WebSocket connection and handles incoming messages.
 
         Parameters
         ----------
@@ -144,22 +192,26 @@ class SingleWsConnection:
             self.logger.info(f"Conn '{self._conn_id}' starting on '{url}'.")
 
             # (WSTransport, WSListener)
-            (_, self._ws_client) = await ws_connect(lambda: RawWsConnection(self._process_frame), url)
+            (_, self._ws_client) = await ws_connect(
+                lambda: RawWsConnection(self._process_frame), url
+            )
 
-            # Dummy empty list incase none provided, never iterates.
+            # Dummy empty list in case none provided, never iterates.
             on_connect = on_connect if on_connect else []
 
             for payload in on_connect:
                 self.send_data(payload)
 
-            self._conn_task = asyncio.create_task(self._ws_client.transport.wait_disconnected())
+            self._conn_task = asyncio.create_task(
+                self._ws_client.transport.wait_disconnected()
+            )
 
         except Exception as e:
             self.logger.error(f"Conn '{self._conn_id}': {e}")
 
     def close(self) -> None:
         """
-        Close the WebSocket connection and cancel any ongoing tasks.
+        Closes the WebSocket connection and cancels any ongoing tasks.
         """
         self.running = False
 
