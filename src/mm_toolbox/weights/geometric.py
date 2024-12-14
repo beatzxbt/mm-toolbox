bb5ef833a92e6218c087566221911f0ffc193293
beatzxbt <PatelV17@cardiff.ac.uk>
2024-12-14 12:55:19 +0000

    add: remove njit

--- src/mm_toolbox/weights/geometric.py
+++ src/mm_toolbox/weights/geometric.py
@@ -1,11 +1,8 @@
 import numpy as np
-from numba import njit
-from numba.types import float64
 from typing import Optional
 
 
-@njit(error_model="numpy", fastmath=True)
-def geometric_weights(num: int, r: Optional[float] = None) -> np.ndarray[float]:
+def geometric_weights(num: int, r: Optional[float] = None) -> np.ndarray:
     """
     Generates a list of `num` weights that follow a geometric distribution and sum to 1.
 
@@ -22,8 +19,10 @@ def geometric_weights(num: int, r: Optional[float] = None) -> np.ndarray[float]:
     np.ndarray
         An array of normalized geometric weights from lowest -> highest.
     """
-    assert num > 1, "Number of weights generated cannot be <1."
-    r = r if r is not None else 0.75
-    weights = np.array([r**i for i in range(num)], dtype=float64)
+    if num <= 1:
+        raise ValueError("Number of weights generated cannot be <1.")
+    
+    r = r if r else 0.75
+    weights = np.array([r**i for i in range(num)], dtype=np.float64)
     normalized = weights / weights.sum()
     return normalized
