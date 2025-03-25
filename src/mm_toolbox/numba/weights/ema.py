98372b025c3d21b02e9039073304826e62da44e9
beatzxbt <PatelV17@cardiff.ac.uk>
2025-03-25 18:36:04 +0000

    add: working numba weights

---
+++ src/mm_toolbox/numba/weights/ema.py
@@ -0,0 +1,32 @@
+import numpy as np
+from typing import Optional
+from numba import njit
+
+@njit(inline="always", error_model="numpy")
+def ema_weights(window: int, alpha: Optional[float] = None) -> np.ndarray:
+    """
+    Calculate EMA (Exponential Moving Average)-like weights for a given window size.
+
+    Parameters
+    ----------
+    window : int
+        The number of periods to use for the EMA calculation.
+
+    alpha : float, optional
+        The decay factor for the EMA calculation. If not provided, it is calculated as 3 / (window + 1).
+
+    Returns
+    -------
+    np.ndarray
+        An array of normalized EMA-like weights from lowest -> highest.
+    """
+    if window <= 1:
+        raise ValueError(f"Invalid window size; expected >1 but got {window}")
+
+    alpha = alpha if alpha else 3.0 / float(window + 1)
+
+    weights = np.array(
+        [alpha * (1.0 - alpha) ** i for i in range(window - 1, -1, -1)],
+        dtype=np.float64,
+    )
+    return weights
---
+++ src/mm_toolbox/numba/weights/geometric.py
@@ -0,0 +1,29 @@
+import numpy as np
+from typing import Optional
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def geometric_weights(num: int, r: Optional[float] = None) -> np.ndarray:
+    """
+    Generates a list of `num` weights that follow a geometric distribution and sum to 1.
+
+    Parameters
+    ----------
+    num : int
+        The number of weights to generate.
+
+    r : float, optional
+        The common ratio of the geometric sequence. Must be strictly between 0 and 1. The default value is 0.75.
+
+    Returns
+    -------
+    np.ndarray
+        An array of normalized geometric weights from lowest -> highest.
+    """
+    if num <= 1:
+        raise ValueError(f"Invalid number of weights; expected > 1 but got {num}.")
+
+    r = r if r else 0.75
+    weights = np.array([r**i for i in range(num)], dtype=np.float64)
+    normalized = weights / weights.sum()
+    return normalized
