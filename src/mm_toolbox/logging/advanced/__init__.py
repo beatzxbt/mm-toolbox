b48313a3ee25ab472f06007c5939fe027e0b7864
beatzxbt <PatelV17@cardiff.ac.uk>
2025-03-29 15:02:05 +0000

    add: completed but broken advanced logger (build failures)

--- src/mm_toolbox/logging/advanced/__init__.py
+++ src/mm_toolbox/logging/advanced/__init__.py
@@ -1,3 +1,25 @@
-# from .config import LoggerConfig as LoggerConfig
-# from .worker import WorkerLogger as WorkerLogger
-# from .master import MasterLogger as MasterLogger
+from .config import LoggerConfig as LoggerConfig
+from .worker import WorkerLogger as WorkerLogger
+from .master import MasterLogger as MasterLogger
+from .structs import LogLevel as LogLevel
+from .handlers import (
+    FileLogHandler as FileLogHandler,
+    DiscordLogHandler as DiscordLogHandler,
+    TelegramLogHandler as TelegramLogHandler,
+    BaseLogHandler as BaseLogHandler,
+    ZMQLogHandler as ZMQLogHandler,
+    TestLogHandler as TestLogHandler,
+)
+
+__all__ = [
+    "LogLevel",
+    "LoggerConfig",
+    "WorkerLogger",
+    "MasterLogger",
+    "FileLogHandler",
+    "DiscordLogHandler",
+    "TelegramLogHandler",
+    "BaseLogHandler",
+    "ZMQLogHandler",
+    "TestLogHandler",
+]
