67e161d90d10901a0c9e216f8fc9d84edd19b6bd
beatzxbt <PatelV17@cardiff.ac.uk>
2025-09-17 13:47:53 +0100

    fix: switch to new pylog and clog

--- src/mm_toolbox/logging/advanced/config.pyx
+++ src/mm_toolbox/logging/advanced/config.pyx
@@ -1,10 +1,11 @@
-from mm_toolbox.logging.advanced.structs import LogLevel
-from mm_toolbox.logging.advanced.structs cimport CLogLevel
+from mm_toolbox.logging.advanced.pylog import PyLogLevel
+from mm_toolbox.logging.advanced.log cimport CLogLevel
+
 
 cdef class LoggerConfig:
     def __init__(
         self, 
-        LogLevel base_level=LogLevel.INFO,
+        object base_level=None,
         bint do_stout=False,
         str str_format="%(asctime)s [%(levelname)s] %(name)s - %(message)s", 
         str path="ipc:///tmp/hft_logger", 
@@ -14,7 +15,7 @@ cdef class LoggerConfig:
         Initialize the LoggerConfig with transport, path, and format settings.
         
         Args:
-            base_level (LogLevel): The minimum log level to process. Defaults to `LogLevel.INFO`.
+            base_level (PyLogLevel): The minimum log level to process. Defaults to `PyLogLevel.INFO`.
             do_stout (bool): Whether to print to stdout. Defaults to False.
             str_format (str): The format string for log messages, similar to Python's default logger.
                 Supports standard format placeholders like %(asctime)s, %(name)s, %(levelname)s, 
@@ -24,7 +25,9 @@ cdef class LoggerConfig:
                 required by the selected transport protocol. Defaults to 'ipc:///tmp/hft_logger'.
             flush_interval_s (float): Timeout in seconds for log messages. Defaults to 1.0.
         """
-        self.set_base_level(base_level)
+        if base_level is None:
+            base_level = PyLogLevel.INFO
+        self.base_level = self.set_base_level_to_clog_level(base_level)
         self.do_stout = do_stout
 
         self.str_format = str_format
@@ -39,20 +42,15 @@ cdef class LoggerConfig:
         if self.flush_interval_s <= 0.0:
             raise ValueError(f"Invalid flush interval; expected a positive number but got '{self.flush_interval_s}'")
 
-    cdef inline void set_base_level(self, LogLevel level):
-        """
-        Maps a LogLevel to a CLogLevel.
-        
-        Args:
-            level (LogLevel): The log level to map.
-        """
-        if level == LogLevel.TRACE:
-            self.base_level = CLogLevel.CTRACE
-        elif level == LogLevel.DEBUG:
-            self.base_level = CLogLevel.CDEBUG
-        elif level == LogLevel.INFO:
-            self.base_level = CLogLevel.CINFO
-        elif level == LogLevel.WARNING:
-            self.base_level = CLogLevel.CWARNING
-        elif level == LogLevel.ERROR:
-            self.base_level = CLogLevel.CERROR
+    cdef inline CLogLevel set_base_level_to_clog_level(self, object level):
+        """Maps a PyLogLevel to a CLogLevel."""
+        if level == PyLogLevel.TRACE:
+            return CLogLevel.TRACE
+        elif level == PyLogLevel.DEBUG:
+            return CLogLevel.DEBUG
+        elif level == PyLogLevel.INFO:
+            return CLogLevel.INFO
+        elif level == PyLogLevel.WARNING:
+            return CLogLevel.WARNING
+        elif level == PyLogLevel.ERROR:
+            return CLogLevel.ERROR
